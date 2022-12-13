457faafd0bee9bcc3de9b689be19cf55934018f5
JH <67933778+jayhigh47@users.noreply.github.com>
2022-12-13 23:51:20 +0900

    React: 7.1 ToDoList

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./7.0_ToDo_App";
+import App from "./7.1_ToDoList_App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
