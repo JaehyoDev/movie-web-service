3c5c7fe5e4b92af8de2b8a9f06935725e06e001b
JH <67933778+jayhigh47@users.noreply.github.com>
2022-12-16 00:46:43 +0900

    React: 7.3 Movie App Part 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./7.2_CoinTracker_App";
+import App from "./7.3_Movie_App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
