b72cffe4df14e65e8f662035d3f1cb1091ef6d05
JH <67933778+jayhigh47@users.noreply.github.com>
2022-12-15 23:31:36 +0900

    React: 7.2 CoinTracker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./7.1_ToDoList_App";
+import App from "./7.2_CoinTracker_App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
